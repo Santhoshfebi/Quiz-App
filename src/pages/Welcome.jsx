cd47eb18cd6621897bf9ca6e6b9cadb3981b1148
Santhoshfebi <santhoshrajr121@gmail.com>
2025-10-08 23:13:29 +0530

    time

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,72 +1,24 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// export default function Welcome() {
-//   const navigate = useNavigate();
-//   const [name, setName] = useState("");
-//   const [phone, setPhone] = useState("");
-//   const [place, setPlace] = useState("");
-
-//   const handleStart = () => {
-//      const phoneRegex = /^[0-9]{10}$/;
-//     if (!name || !phone || !place) {
-//       alert("Please fill all fields");
-//       return;
-//     }
-//     if (!phoneRegex.test(phone)) {
-//       alert("Please enter a valid 10-digit phone number");
-//       return;
-//     }
-//     navigate("/quiz", { state: { name, phone, place } });
-    
-//   };
-
-//   return (
-//     <div className="mx-auto mt-20 w-96 bg-white p-6 rounded-2xl shadow-lg space-y-4">
-//       <h1 className="text-2xl font-bold text-center">Welcome to the Quiz!</h1>
-//       <p className="text-center">Enjoy the Quiz...!</p>
-//       <h5 className="text-xl font-bold text-center">Chapter: Mock Round</h5>
-//       <input
-//         type="text"
-//         placeholder="Your Name"
-//         value={name}
-//         onChange={(e) => setName(e.target.value)}
-//         className="w-full border px-3 py-2 rounded-lg "
-//       />
-//       <input
-//       type="tel"
-//         placeholder="Phone Number"
-//         value={phone}
-//         onChange={(e) => setPhone(e.target.value)}
-//         className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-//         maxLength={10}/>
-//       <input
-//       type="text" 
-//         placeholder="Division"
-//         value={place}
-//         onChange={(e) => setPlace(e.target.value)}
-//         className="w-full border px-3 py-2 rounded-lg "
-//       />
-//       <button
-//         onClick={handleStart}
-//         className="w-full bg-blue-400 text-white py-2 rounded-lg hover:bg-green-600"
-//       >
-//         Start Quiz
-//       </button>
-//     </div>
-//   );
-// }
-
-
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 
 export default function Welcome() {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [place, setPlace] = useState("");
   const navigate = useNavigate();
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const scheduledTime = new Date("2025-10-08T18:00:00"); // YYYY-MM-DDTHH:MM:SS
+  const [isQuizAvailable, setIsQuizAvailable] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      setCurrentTime(now);
+      setIsQuizAvailable(now >= scheduledTime);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   const handleStart = () => {
     const phoneRegex = /^[0-9]{10}$/;
